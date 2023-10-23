6832bc67697ac4cf0d08afc3ee9468be548f7ce6
theambermustafa <tptb123666@gmail.com>
2023-10-23 19:43:30 +0530

    Nothing happens until you subscribe

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,27 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux().subscribe(System.out::println);
+        //or we could have also used: .subscribe(name -> {
+        // System.out.println(name);
+        // });
+
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> {
+                    System.out.println("The name is: " + name);
+                });
+
+        //NOTE: Nothing happens until you subscribe
+    }
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("Alex", "Ben", "Chloe")); //comes either from a remote service or db eventually
+    }
 }
