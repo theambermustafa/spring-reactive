e033065fd2063e8626109e15f937a99498f97e97
theambermustafa <tptb123666@gmail.com>
2023-10-23 19:49:00 +0530

    Nothing happens until you subscribe

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,27 +1,36 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public class FluxAndMonoGeneratorService {
 
     public static void main(String[] args) {
-        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
-        fluxAndMonoGeneratorService.namesFlux().subscribe(System.out::println);
+        FluxAndMonoGeneratorService generatorService = new FluxAndMonoGeneratorService();
+        generatorService.namesFlux().subscribe(System.out::println);
         //or we could have also used: .subscribe(name -> {
         // System.out.println(name);
         // });
 
-        fluxAndMonoGeneratorService.namesFlux()
+        generatorService.namesFlux()
                 .subscribe(name -> {
                     System.out.println("The name is: " + name);
                 });
 
         //NOTE: Nothing happens until you subscribe
+
+        generatorService.namesMono().subscribe(System.out::println);
+        generatorService.namesMono().subscribe(x -> System.out.println("The name is: " + x));
+
     }
 
     public Flux<String> namesFlux() {
         return Flux.fromIterable(List.of("Alex", "Ben", "Chloe")); //comes either from a remote service or db eventually
+    }
+
+    public Mono<String> namesMono() {
+        return Mono.just("Amber");
     }
 }
