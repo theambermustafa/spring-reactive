a2802fcce033d8e5608affbf7629d98f0e81c668
theambermustafa <tptb123666@gmail.com>
2023-10-29 16:42:08 +0530

    Alright

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -9,14 +9,16 @@ public class FluxAndMonoGeneratorService {
 
     public static void main(String[] args) {
         FluxAndMonoGeneratorService generatorService = new FluxAndMonoGeneratorService();
-//        generatorService.namesFlux().subscribe(System.out::println);
-        //or we could have also used: .subscribe(name -> {
-        // System.out.println(name);
+        //generatorService.namesFlux()
+        //      .subscribe(System.out::println);
+        //or we could have also used:
+        //      .subscribe(name -> {
+        //          System.out.println(name);
         // });
 
         generatorService.namesFlux()
                 .subscribe(name -> {
-                    System.out.println("The name is: " + name);
+                    System.out.println("The name is: " + name); //this is consuming the data
                 });
 
         //NOTE: Nothing happens until you subscribe
@@ -35,10 +37,20 @@ public class FluxAndMonoGeneratorService {
         generatorService.requiredNames().subscribe(x -> {
             System.out.println("Required name: " + x);
         });
+
+        //flatMap() example
+        generatorService.namesFlux_flatMap(3).subscribe(System.out::print);
     }
+
 
     private static boolean isLengthFine(String str) {
         return str.length() > 5;
+    }
+
+    //ALEX -> Flux(A,L,E,X)
+    public static Flux<String> splitString(String name) {
+        var splittedName = name.split("");
+        return Flux.fromArray(splittedName);
     }
 
     public Flux<String> namesFlux() {
@@ -67,5 +79,14 @@ public class FluxAndMonoGeneratorService {
         return Flux.fromIterable(List.of("Amber", "Mustafa", "Hesoyam", "Chloe", "La Martiniere", "Alex"))
                 .filter(FluxAndMonoGeneratorService::isLengthFine)
                 .log();
+    }
+
+    public Flux<String> namesFlux_flatMap(int stringLength) {
+        return Flux.fromIterable(List.of("Alex", "Ben", "Chloe"))
+                .filter(name -> name.length() > stringLength)
+                .map(String::toUpperCase)
+                //We need A,L,E,X,C,H,L,O,E
+                .flatMap(FluxAndMonoGeneratorService::splitString);
+//                .log();
     }
 }
